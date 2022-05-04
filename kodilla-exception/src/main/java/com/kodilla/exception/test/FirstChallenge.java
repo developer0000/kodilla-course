fc12b320d8fac5c08126fcfd374a1e5c032a6def
Paul <paulpauls007@gmail.com>
2022-05-04 10:00:05 +0100

    third commit

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,2 +1,38 @@
-package com.kodilla.exception.test;public class FirstChallenge {
+package com.kodilla.exception.test;
+
+import java.io.IOException;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("Oh no! Something went wrong! Error:" + e);
+        } finally {
+            System.out.println("I am gonna be here... always!");
+
+        }
+
+
+
+    }
+
 }
+
+
