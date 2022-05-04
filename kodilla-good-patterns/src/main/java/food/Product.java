e25270c14ed3eac6d754b783b1156aa04db46e7a
Paul <paulpauls007@gmail.com>
2022-05-04 09:24:10 +0100

    javaPatterns2

--- kodilla-good-patterns/src/main/java/food/Product.java
+++ kodilla-good-patterns/src/main/java/food/Product.java
@@ -1,6 +1,7 @@
 package food;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
 
@@ -8,9 +9,25 @@ public class Product {
 
     BigDecimal price;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name) &&
+                Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
 
     public Product(String name, BigDecimal price) {
         this.name = name;
         this.price = price;
     }
 }
+
+
+
